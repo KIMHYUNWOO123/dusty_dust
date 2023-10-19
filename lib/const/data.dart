15c68b3c9c0c685ea40d412da89004364807c0b8
김현우 <wry1004@cocen.com>
2023-10-20 07:54:26 +0900

    23/10/20

---
+++ lib/const/data.dart
@@ -0,0 +1,1 @@
+const serviceKey = 'Y5Y9UF+VlUOxmsREIAx45j7XOKpWYPZaZhDq1+hx6CO+1t0C6r5zfgC3LdmSuFbBbc0m/RLhZ0eJZdZKO0eqtA==';
--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:dio/dio.dart';
 import 'package:dusty_dust/component/category_card.dart';
 import 'package:dusty_dust/component/main_app_bar.dart';
 import 'package:dusty_dust/component/main_drawer.dart';
@@ -5,9 +6,37 @@ import 'package:flutter/material.dart';
 
 import '../component/hourly_card.dart';
 import '../const/colors.dart';
+import '../const/data.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    fetchData();
+  }
+
+  fetchData() async {
+    final response = await Dio().get(
+      'http://apis.data.go.kr/B552584/ArpltnStatsSvc/getCtprvnMesureLIst',
+      queryParameters: {
+        'serviceKey': serviceKey,
+        'return': 'json',
+        'numOfRows': 30,
+        'pageNo': 1,
+        'itemCode': 'PM10',
+        'dataGubun': 'HOUR',
+        'searchCondition': 'WEEK',
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
