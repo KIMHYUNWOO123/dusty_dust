8106cce1c239ce27422dae360484127a5b810002
김현우 <wry1004@cocen.com>
2023-10-23 08:18:45 +0900

    23/10/23

--- lib/component/main_app_bar.dart
+++ lib/component/main_app_bar.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
 
-import '../const/colors.dart';
+import '../model/stat_model.dart';
+import '../model/status_model.dart';
 
 class MainAppBar extends StatelessWidget {
-  const MainAppBar({super.key});
+  final StatusModel status;
+  final StatModel stat;
+
+  const MainAppBar({required this.status, required this.stat, super.key});
 
   @override
   Widget build(BuildContext context) {
     final ts = TextStyle(color: Colors.white, fontSize: 30.0);
     return SliverAppBar(
-      backgroundColor: primaryColor,
+      backgroundColor: status.primaryColor,
       expandedHeight: 500,
       flexibleSpace: FlexibleSpaceBar(
         background: SafeArea(
@@ -19,31 +23,37 @@ class MainAppBar extends StatelessWidget {
               children: [
                 Text(
                   '서울',
-                  style: ts.copyWith(
-                      fontSize: 40.0, fontWeight: FontWeight.w700),
+                  style:
+                      ts.copyWith(fontSize: 40.0, fontWeight: FontWeight.w700),
                 ),
                 Text(
-                  DateTime.now().toString(),
+                  getTimeFromDateTime(dateTime: stat.dataTime),
                   style: ts.copyWith(
                     fontSize: 20.0,
                   ),
                 ),
-                const SizedBox(height: 20.0,),
+                const SizedBox(
+                  height: 20.0,
+                ),
                 Image.asset(
-                  'asset/img/mediocre.png',
+                  status.imagePath,
                   width: MediaQuery.of(context).size.width / 2,
                 ),
-                const SizedBox(height: 20.0,),
-                Text(
-                  '보통',
-                  style: ts.copyWith(
-                      fontSize: 40.0, fontWeight: FontWeight.w700),
+                const SizedBox(
+                  height: 20.0,
                 ),
-                const SizedBox(height: 8.0,),
                 Text(
-                  '나쁘지 않네요!',
-                  style: ts.copyWith(
-                      fontSize: 20.0, fontWeight: FontWeight.w700),
+                  status.label,
+                  style:
+                      ts.copyWith(fontSize: 40.0, fontWeight: FontWeight.w700),
+                ),
+                const SizedBox(
+                  height: 8.0,
+                ),
+                Text(
+                  status.comment,
+                  style:
+                      ts.copyWith(fontSize: 20.0, fontWeight: FontWeight.w700),
                 ),
               ],
             ),
@@ -51,5 +61,13 @@ class MainAppBar extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  String getTimeFromDateTime({required DateTime dateTime}) {
+    return '${dateTime.year}-${dateTime.month}-${dateTime.day} ${getTimeFormat(dateTime.hour)}:${getTimeFormat(dateTime.minute)}';
+  }
+
+  String getTimeFormat(int number) {
+    return number.toString().padLeft(2, '0');
   }
 }
--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,12 +1,13 @@
-import 'package:dio/dio.dart';
 import 'package:dusty_dust/component/category_card.dart';
 import 'package:dusty_dust/component/main_app_bar.dart';
 import 'package:dusty_dust/component/main_drawer.dart';
+import 'package:dusty_dust/const/status_level.dart';
+import 'package:dusty_dust/repository/stat_repository.dart';
 import 'package:flutter/material.dart';
 
 import '../component/hourly_card.dart';
 import '../const/colors.dart';
-import '../const/data.dart';
+import '../model/stat_model.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -16,26 +17,9 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  @override
-  void initState() {
-    super.initState();
-
-    fetchData();
-  }
-
-  fetchData() async {
-    final response = await Dio().get(
-      'http://apis.data.go.kr/B552584/ArpltnStatsSvc/getCtprvnMesureLIst',
-      queryParameters: {
-        'serviceKey': serviceKey,
-        'return': 'json',
-        'numOfRows': 30,
-        'pageNo': 1,
-        'itemCode': 'PM10',
-        'dataGubun': 'HOUR',
-        'searchCondition': 'WEEK',
-      },
-    );
+  Future<List<StatModel>> fetchData() async {
+    final statModels = await StatRepository.fetchData();
+    return statModels;
   }
 
   @override
@@ -43,21 +27,46 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       backgroundColor: primaryColor,
       drawer: MainDarwer(),
-      body: CustomScrollView(
-        slivers: [
-          MainAppBar(),
-          SliverToBoxAdapter(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                CategoryCard(),
-                const SizedBox(height: 16.0),
-                HourlyCard(),
+      body: FutureBuilder<List<StatModel>>(
+          future: fetchData(),
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return Center(
+                child: Text('에러가 있습니다.'),
+              );
+            }
+
+            if (!snapshot.hasData) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+
+            List<StatModel> stats = snapshot.data!;
+            StatModel recentStat = stats[0];
+
+            final status = statusLevel.where(
+              (element) => element.minFineDust < recentStat.seoul,
+            ).last;
+            return CustomScrollView(
+              slivers: [
+                MainAppBar(
+                  status: status,
+                  stat: recentStat,
+                ),
+                SliverToBoxAdapter(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      CategoryCard(),
+                      const SizedBox(height: 16.0),
+                      HourlyCard(),
+                    ],
+                  ),
+                ),
               ],
-            ),
-          ),
-        ],
-      ),
+            );
+          }),
     );
   }
 }
